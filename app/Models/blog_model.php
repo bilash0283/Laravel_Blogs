ef9f9ce77433bcfb03330edf0fd6ea617f394ae4
bilash0283 <bilash0283@gmail.com>
2025-03-24 16:26:05 +0600

    blog model modify

--- app/Models/blog_model.php
+++ app/Models/blog_model.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use GuzzleHttp\Promise\Create;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class blog_model extends Model
 {
     public function store_blog_data($blog)
     {
-        Create::class->all();
+        DB::table('blogs')->insert([
+            'title' => $blog->title,
+            'description' => $blog->description,
+            'image' => $blog->image,
+            'status' => $blog->status,
+        ]);
     }
 }
